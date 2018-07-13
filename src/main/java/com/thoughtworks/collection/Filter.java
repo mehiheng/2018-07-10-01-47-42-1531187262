69b8852c66d1e5b87e258765a5bf257aba38a25c
ZHOUAD <Jing690518>
2018-07-13 16:42:24 +0800

    filter

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,22 +11,47 @@ public class Filter {
     List<Integer>  array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+            this.array=array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList();
+        for (int element : array) {
+            if (element % 2 == 0) {
+                result.add(element);
+            }
+        }
+        return result;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList();
+        for (int element : array) {
+            if (element % 3 == 0) {
+                result.add(element);
+            }
+        }
+        return result;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList();
+        for(int f:firstList){
+            if(secondList.contains(f)){
+                result.add(f);
+            }
+        }
+        return result;
     }
 
+
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList();
+        for (int a : array) {
+            if(!result.contains(a)){
+                result.add(a);
+            }
+        }
+        return result;
     }
 }
