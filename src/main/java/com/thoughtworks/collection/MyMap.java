2e4d599ea27e953bcdba53fc079f037d150a1230
ZHOUAD <Jing690518>
2018-07-13 20:39:06 +0800

    finished

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,18 +24,34 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        return array.stream().map(x->String.fromCharCode(x+96))
+        List<String> result = new ArrayList();
+        for(int i=0;i<array.size();i++){
+            int a=array.get(i);
+            result.add(Character.valueOf((char)(a+96)).toString());
+        }
+        return result;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> result = new ArrayList();
+        for(int i=0;i<array.size();i++){
+            int a=array.get(i);
+            if(a>52){
+                result.add("b"+Character.valueOf((char)(a+96-52)).toString());
+            }else if(a>26){
+                result.add("a"+Character.valueOf((char)(a+96-26)).toString());
+            }else{
+                result.add(Character.valueOf((char)(a+96)).toString());
+            }
+        }
+        return result;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
 }
--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,9 +2,11 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class Reduce {
+public class Reduce implements SingleLink{
 
     List<Integer> arrayList;
 
@@ -13,42 +15,87 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(Integer::max).get();
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        return arrayList.stream().reduce(Integer::min).get();
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+//        return arrayList.stream().mapToDouble(num -> num).average().getAsDouble();
+        double sum=0;
+        for(int element :arrayList){
+            sum+=element;
+    }
+    return sum/arrayList.size();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        int index = arrayList.size() / 2;
+        if (index % 2 == 0) {
+            return arrayList.stream().skip(index - 1).limit(2).mapToInt(num -> num).average().getAsDouble();
+        } else {
+            return arrayList.stream().skip(index).findFirst().get();
+        }
     }
 
+
     public int getFirstEven() {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(x->x%2==0).findFirst().get();
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        int firstEvent = arrayList.stream().filter(num -> num % 2 == 0).findFirst().get();
+        return arrayList.indexOf(firstEvent);
     }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+    public boolean isEqual(List<Integer> objectList) {
+//        Boolean isEqual = false;
+//        if (objectList.size() == arrayList.size()) {
+//            isEqual = IntStream.range(0, arrayList.size())
+//                    .allMatch(index -> arrayList.get(index) == objectList.get(index));
+//        }
+//        return isEqual;
+        List<Integer> result=new ArrayList();
+        for(Integer a:objectList){
+            if(arrayList.contains(a)){
+                result.add(a);
+            }
+        }
+            if(result.size()==arrayList.size()){
+            return true;
+        }else{
+            return false;
+            }
     }
 
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
-    }
+            int index = arrayList.size() / 2;
+            if (arrayList.size() % 2 == 0) {
+                return (Double.valueOf(singleLink.getNode(index) + singleLink.getNode(index + 1)) / 2);
+            } else {
+                return Double.valueOf(singleLink.getNode(index + 1));
+            }
+        }
+
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(num -> num % 2 != 0).reduce((first, second) -> second).get();
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.lastIndexOf(getLastOdd());
+    }
+
+
+    @Override
+    public void addTailPointer(Object item) {
+
+    }
+
+    @Override
+    public int getNode(int index) {
+        return Integer.parseInt(null);
     }
 }
