2db5ec853f9156cc71f1c2a0f1b894750a199073
ZHOUAD <Jing690518>
2018-07-13 17:17:57 +0800

    flaten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
@@ -12,10 +14,28 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result=new ArrayList() ;
+       for(int i=0;i<array.length;i++){
+           for(int n=0;n<array[i].length;n++){
+               result.add(array[i][n]);
+           }
+       }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result=new ArrayList() ;
+        List<Integer> arrayList=new ArrayList() ;
+        for(int i=0;i<array.length;i++){
+            for(int n=0;n<array[i].length;n++){
+                result.add(array[i][n]);
+            }
+        }
+        for(int element:result){
+            if(!arrayList.contains(element)){
+                arrayList.add(element);
+            }
+        }
+        return arrayList;
     }
 }
